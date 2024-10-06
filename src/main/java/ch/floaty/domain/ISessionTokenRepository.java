dad29b200887f5215e2e72a75a39e1ea5714cc15
matthaeusheer <matthaeusheer@gmail.com>
2024-10-06 22:51:23 +0200

    added integration tests for register and login flow

--- src/main/java/ch/floaty/domain/ISessionTokenRepository.java
+++ src/main/java/ch/floaty/domain/ISessionTokenRepository.java
@@ -2,6 +2,6 @@ package ch.floaty.domain;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface ISessionTokenRepository extends CrudRepository<SessionToken, String> {
+public interface ISessionTokenRepository extends CrudRepository<SessionToken, Long> {
     SessionToken findByToken(String token);
 }
