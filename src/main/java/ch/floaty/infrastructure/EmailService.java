eebe04ec609e6ae9f7cf9b1bee43b5f00a81f643
matthaeusheer <matthaeusheer@gmail.com>
2024-11-01 17:34:13 +0100

    added greenmail integration tests and sendgrid email sending

--- src/main/java/ch/floaty/infrastructure/EmailService.java
+++ src/main/java/ch/floaty/infrastructure/EmailService.java
@@ -1,5 +1,6 @@
 package ch.floaty.infrastructure;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -8,18 +9,20 @@ import org.springframework.stereotype.Service;
 public class EmailService {
 
     private final JavaMailSender mailSender;
-    public static final String FROM = "noreply@floaty.com";
+    private String from;
 
-    public EmailService(JavaMailSender mailSender) {
+    public EmailService(JavaMailSender mailSender, @Value("${email.from}") String from) {
         this.mailSender = mailSender;
+        this.from = from;
     }
 
     public void sendSimpleEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(FROM);
+        message.setFrom(from);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
         mailSender.send(message);
     }
 }
+
